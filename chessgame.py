4c96612444c46cdc4a5deca0197cd5463452ac40
jumel <alexis.jumel1@gmail.com>
2024-02-07 17:29:24 +0100

    Moify

--- chessgame.py
+++ chessgame.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class chessGame:
-    board = ""
+
     class Type(Enum):
         BKING = '♚'
         BRIDER = '♞'
@@ -16,39 +16,34 @@ class chessGame:
         WCRAZY = '♗'
         WPAWN = '♙'
     
-    def CreateBoard(self):
-        self._board = []
+    def CreateBoard():
+        board = []
         for _ in range(8):
             row = [' '] *8
-            self._board.append(row) 
-        return self._board
+            board.append(row) 
+        return board
 
     def PrintBoard(self):
-        for row in self._board:
+        for row in self.board:
             print(row)
     
-    def CreateTeam(self):
-        self._board = chessGame.CreateBoard()
+    def CreateTeam():
+        board = chessGame.CreateBoard()
 
         whiteTeam = []
         whiteTeam.append([chessGame.Type.WTOWER.value,chessGame.Type.WRIDER.value,chessGame.Type.WCRAZY.value,chessGame.Type.WQUEEN.value,chessGame.Type.WKING.value,chessGame.Type.WCRAZY.value,chessGame.Type.WRIDER.value,chessGame.Type.WTOWER.value])
         whiteTeam.append([chessGame.Type.WPAWN.value]*8)
         
-        self._board[0] = whiteTeam[0]
-        self._board[1] = whiteTeam[1]
+        board[0] = whiteTeam[0]
+        board[1] = whiteTeam[1]
 
         blackTeam = []
         blackTeam.append([chessGame.Type.BTOWER.value,chessGame.Type.BRIDER.value,chessGame.Type.BCRAZY.value,chessGame.Type.BQUEEN.value,chessGame.Type.BKING.value,chessGame.Type.BCRAZY.value,chessGame.Type.BRIDER.value,chessGame.Type.BTOWER.value])
         blackTeam.append([chessGame.Type.BPAWN.value]*8)
 
-        self._board[6] = blackTeam[1]
-        self._board[7] = blackTeam[0]
+        board[6] = blackTeam[1]
+        board[7] = blackTeam[0]
 
-        return self._board
-
-        
-chessgame = chessGame 
-print(chessgame.CreateBoard(chessgame))
-print(chessGame.CreateTeam(chessgame))
+        return board
 
 
