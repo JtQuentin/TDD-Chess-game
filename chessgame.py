eac39b9df4de61b6ad11a81064d9ad34ccf8c4fb
jumel <alexis.jumel1@gmail.com>
2024-02-07 17:12:43 +0100

    Modify Board

--- chessgame.py
+++ chessgame.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class chessGame:
+    board = ""
     class Type(Enum):
         BKING = '♚'
         BRIDER = '♞'
@@ -15,37 +16,38 @@ class chessGame:
         WCRAZY = '♗'
         WPAWN = '♙'
     
-    def CreateBoard():
-        board = []
+    def CreateBoard(self):
+        self._board = []
         for _ in range(8):
             row = [' '] *8
-            board.append(row) 
-        return board
+            self._board.append(row) 
+        return self._board
 
-    def PrintBoard():
-        for row in chessGame.CreateBoard():
+    def PrintBoard(self):
+        for row in self._board:
             print(row)
     
-    def CreateTeam():
-        board = chessGame.CreateBoard()
+    def CreateTeam(self):
+        self._board = chessGame.CreateBoard()
 
         whiteTeam = []
         whiteTeam.append([chessGame.Type.WTOWER.value,chessGame.Type.WRIDER.value,chessGame.Type.WCRAZY.value,chessGame.Type.WQUEEN.value,chessGame.Type.WKING.value,chessGame.Type.WCRAZY.value,chessGame.Type.WRIDER.value,chessGame.Type.WTOWER.value])
         whiteTeam.append([chessGame.Type.WPAWN.value]*8)
         
-        board[0] = whiteTeam[0]
-        board[1] = whiteTeam[1]
+        self._board[0] = whiteTeam[0]
+        self._board[1] = whiteTeam[1]
 
         blackTeam = []
         blackTeam.append([chessGame.Type.BTOWER.value,chessGame.Type.BRIDER.value,chessGame.Type.BCRAZY.value,chessGame.Type.BQUEEN.value,chessGame.Type.BKING.value,chessGame.Type.BCRAZY.value,chessGame.Type.BRIDER.value,chessGame.Type.BTOWER.value])
         blackTeam.append([chessGame.Type.BPAWN.value]*8)
 
-        board[6] = blackTeam[1]
-        board[7] = blackTeam[0]
+        self._board[6] = blackTeam[1]
+        self._board[7] = blackTeam[0]
 
-        return board
+        return self._board
         
-# print(chessGame.CreateBoard())
-print(chessGame.CreateTeam())
+chessgame = chessGame 
+print(chessgame.CreateBoard(chessgame))
+print(chessGame.CreateTeam(chessgame))
 
 
