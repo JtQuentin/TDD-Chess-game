03c44739f3fc7c463f92ffb74cf18e1458e74d1c
jumel <alexis.jumel1@gmail.com>
2024-02-07 15:52:41 +0100

    Create Enum Pion + Test

--- chessgame.py
+++ chessgame.py
@@ -1,10 +1,26 @@
+from enum import Enum
+
 class chessGame:
-    def CreatePion(value):
-        pion = []
-        if(value == 0):
-            pion = ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜", "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"]
-        elif(value == 1):
-            pion = ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
-        else:
-            return None
-        return pion
+    class Type(Enum):
+        BKING = '♚'
+        BRIDER = '♞'
+        BQUEEN = '♛'
+        BTOWER = '♜'
+        BCRAZY = '♝'
+        BPAWN = '♟'
+        WKING = '♔'
+        WRIDER = '♘'
+        WQUEEN = '♕'
+        WTOWER = '♖'
+        WCRAZY = '♗'
+        WPAWN = '♙'
+        
+    # def CreatePion(value):
+    #     pion = []
+    #     if(value == 0):
+    #         pion = [Type.WKING, "♞", "♝", "♛", "♚", "♝", "♞", "♜", "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"]
+    #     elif(value == 1):
+    #         pion = ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
+    #     else:
+    #         return None
+    #     return pion
--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 from chessgame import chessGame
 
-def test_CreatePion():
+# def test_CreatePion():
+#     assert chessGame.CreatePion(0) == ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜", "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"]
+#     assert chessGame.CreatePion(1) == ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
+#     assert chessGame.CreatePion(78) == None
 
-    assert chessGame.CreatePion(0) == ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜", "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"]
-    assert chessGame.CreatePion(1) == ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
-    assert chessGame.CreatePion(78) == None
+def test_ShowPion():
+    assert chessGame.Type.BCRAZY.value == '♝'
+    assert chessGame.Type.BKING.value == '♚'
+    assert chessGame.Type.BPAWN.value == '♟'
+    assert chessGame.Type.BQUEEN.value == '♛'
+    assert chessGame.Type.BRIDER.value == '♞'
+    assert chessGame.Type.BTOWER.value == '♜'
+    assert chessGame.Type.WCRAZY.value == '♗'
+    assert chessGame.Type.WKING.value == '♔'
+    assert chessGame.Type.WPAWN.value == '♙'
+    assert chessGame.Type.WQUEEN.value == '♕'
+    assert chessGame.Type.WRIDER.value == '♘'
+    assert chessGame.Type.WTOWER.value == '♖'
